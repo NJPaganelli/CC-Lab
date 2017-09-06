c94cb9cae96a3cdc0dd9813a27fe804d82bdc264
NJPaganelli <30537208+NJPaganelli@users.noreply.github.com>
2017-09-06 16:55:24 -0400

    Add files via upload
    Credit for the code/tutorial help :https://www.arduino.cc/en/tutorial/switch
    
    I understand all the logic in this code except for the line:
    
    digitalWrite(outputPin, state);
    
    Does it relate to the 'digitalRead' line?

---
+++ lightswitch_test.ino
@@ -0,0 +1,37 @@
+int inputPin = 2;
+int outputPin = 13;
+int state = HIGH;
+int reading;
+int previous = LOW;
+
+long time = 0;
+long debounce = 200;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(inputPin, INPUT);
+  pinMode(outputPin, OUTPUT);
+}
+
+void loop() {
+
+  reading = digitalRead(inputPin);
+//  buttonState = digitalRead(buttonPin);
+  if(reading == HIGH && previous == LOW && millis() - time>debounce){
+    if(state == HIGH)
+      state = LOW;
+     else
+      state = HIGH;
+
+      time = millis();
+  }
+      digitalWrite(outputPin, state); 
+      previous = reading;
+}
+      
+      
+//  delay(75);
+//  }else{                              
+//  digitalWrite(LED_BUILTIN, LOW);    
+//  delay(100);                      
+//}
